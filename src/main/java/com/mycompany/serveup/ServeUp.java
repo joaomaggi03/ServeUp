c820674c685012b4c989b5c1d93b1274be274a36
VictorArxujo <142849807+VictorArxujo@users.noreply.github.com>
2025-05-28 16:49:01 -0300

    Atualizando o ServeUp para o Banco de Dados

--- src/main/java/com/mycompany/serveup/ServeUp.java
+++ src/main/java/com/mycompany/serveup/ServeUp.java
@@ -3,14 +3,37 @@
  */
 
 package com.mycompany.serveup;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
-/**
- *
- * @author Aluno
- */
 public class ServeUp {
-
+    
+    static Connection conn = null;
+    static String url = "jdbc:postgresql://localhost:5432/Aula1";
+    static String user = "postgres";
+    static String senha = "utfpr";
+    static String driver = "org.postgresql.Driver";
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+       try{ //bloco para registro do driver
+           System.out.println("Carregando o Driver");
+           Class.forName(driver);
+           System.out.println("Driver carregado com sucesso");
+       }catch(Exception e){
+           System.out.println("Falha no carregamento do driver");
+       }
+       
+       
+       
+       try{ //bloco para conexão
+           System.out.println("Conectando ao bd");
+           conn = DriverManager.getConnection(url,user,senha);
+           System.out.println("Banco conectado com sucesso");
+           
+       }catch(Exception e){
+           System.out.println("Falha da conexao");
+       }
     }
 }
